79fdb52989b756bfee7a5ed9a0224b07ed2366d2
bymse <egorstrelov@gmail.com>
2025-03-16 16:12:22 +0300

    EF Core diagnostics infra

---
+++ DataLayer/Telemetry/EFCoreDiagnosticEventsObserver.cs
@@ -0,0 +1,18 @@
+﻿namespace DataLayer.Telemetry;
+
+internal class EFCoreDiagnosticEventsObserver : IObserver<KeyValuePair<string, object?>>
+{
+    public void OnNext(KeyValuePair<string, object?> value)
+    {
+        var name = value.Key;
+        var payload = value.Value;
+    }
+
+    public void OnCompleted()
+    {
+    }
+
+    public void OnError(Exception error)
+    {
+    }
+}
---
+++ DataLayer/Telemetry/EFCoreDiagnosticSourceObserver.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.Telemetry;
+
+public class EFCoreDiagnosticSourceObserver : IDisposable, IObserver<DiagnosticListener>
+{
+    private readonly Lock subscriptionLock = new();
+    private IDisposable? subscription;
+    private IDisposable? allSourcesSubscription;
+    private long disposed;
+
+    public void Subscribe()
+    {
+        allSourcesSubscription ??= DiagnosticListener.AllListeners.Subscribe(this);
+    }
+
+    public void OnNext(DiagnosticListener value)
+    {
+        if (Interlocked.Read(ref disposed) == 0 && value.Name == DbLoggerCategory.Name)
+        {
+            lock (subscriptionLock)
+            {
+                subscription?.Dispose();
+                subscription = value.Subscribe(new EFCoreDiagnosticEventsObserver());
+            }
+        }
+    }
+
+    public void OnCompleted()
+    {
+    }
+
+    public void OnError(Exception error)
+    {
+    }
+
+    public void Dispose()
+    {
+        if (Interlocked.CompareExchange(ref disposed, 1, 0) == 1)
+        {
+            return;
+        }
+
+        lock (subscriptionLock)
+        {
+            subscription?.Dispose();
+        }
+
+
+        allSourcesSubscription?.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
