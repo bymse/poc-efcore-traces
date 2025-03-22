a1dbae5b2727d06abd257fccf6c67e4aa751eefb
bymse <egorstrelov@gmail.com>
2025-03-22 22:02:41 +0300

    Load generator

---
+++ LoadGenerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using NBomber.CSharp;
+
+var httpClient = new HttpClient();
+
+var scenario = Scenario.Create("scenario", async context =>
+    {
+        var result = await httpClient.GetAsync("http://localhost:5010/orders?take=5000");
+        if (!result.IsSuccessStatusCode)
+        {
+            return Response.Fail(result.StatusCode);
+        }
+
+        return Response.Ok();
+    })
+    .WithoutWarmUp()
+    .WithLoadSimulations(
+        Simulation.KeepConstant(10, TimeSpan.FromMinutes(10))
+    );
+
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .Run();
