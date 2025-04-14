b15e8c872caceeb0c24621370ba3df77ef38859a
bymse <egorstrelov@gmail.com>
2025-04-14 09:43:38 +0300

    Index page with links

--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -3,17 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Web.Pages;
 
-public class IndexModel : PageModel
+public class IndexModel() : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    public string GrafanaWebMonitoringUrl { get; } = "http://localhost:8080/grafana/d/eeiiievmiry80f/web-monitoring";
+    public string GrafanaRedMetricsUrl { get; } = "http://localhost:8080/grafana/d/beienxwtq39c0a/r-e-d-metrics-cool";
+    public string GrafanaTracesSearchUrl { get; } = "http://localhost:8080/grafana/explore";
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public void OnGet()
     {
-        _logger = logger;
-    }
-
-    public IActionResult OnGet()
-    {
-        return RedirectToPage("/Customers");
+        
     }
 }
